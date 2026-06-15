e4d447f89cdb54e010b152239c6b88a25225d1cb
Bhargav-Krishnamurthy <bhargavkrish18@gmail.com>
2026-06-15 05:50:53 -0400

    parameterised johnson counter done with proper outputs

--- Q5 - Parameterised Johnson Counter/main.v
+++ Q5 - Parameterised Johnson Counter/main.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ns
 
 // synchronous reset
-module johnsoncounter (
+module johnsoncounter #(parameter N = 4) (
   input wire clk,
   input wire rst,
-  output reg [3:0] out
+  output reg [N-1:0] out
 );
   
   always @(posedge clk) begin
-    if(rst) out <= 4'b0000;
+    if(rst) out <= {N{1'b0}};
     else begin
-      out <= {~out[0], out[3:1]};
+      out <= {~out[0], out[N-1:1]};
     end
   end
   
@@ -20,11 +20,12 @@ endmodule
 
 // Testbench
 module johnsoncountertb;
+  localparam N = 4;
   reg clk=0;
   reg rst;
-  wire [3:0] out;
+  wire [N-1:0] out;
   
-  johnsoncounter uut(.clk(clk), .rst(rst), .out(out));
+  johnsoncounter #(N) uut(.clk(clk), .rst(rst), .out(out));
   
   always begin
     #5;
@@ -33,6 +34,8 @@ module johnsoncountertb;
 
   
   initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, johnsoncountertb);
     rst = 1'b1;
     $monitor("Time=%0t | rst=%b | out=%b", $time, rst, out);
     #11;
